4e1a3388055d6010c1e490f94ef5c61902ccc5af
Shweta-Deshpande <49728296+Shweta-Deshpande@users.noreply.github.com>
2025-10-15 14:01:18 -0700

    Add Terraform configuration for Docker Nginx setup

---
+++ main.tf
@@ -0,0 +1,26 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.name
+  name  = "nginx"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
