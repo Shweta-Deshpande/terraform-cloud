873cfd3e4dd4fc2dcc18e2535770b20403883440
Shweta-Deshpande <49728296+Shweta-Deshpande@users.noreply.github.com>
2025-10-16 14:16:14 -0700

    Refactor Terraform config to remove Docker resources
    Removed Docker provider and resources, added random integer and local file resources.

--- main.tf
+++ main.tf
@@ -3,24 +3,21 @@
 
 terraform {
   required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
     }
   }
 }
-
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
+resource "random_integer" "number" {
+  min = 1
+  max = 10000
 }
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.name
-  name  = "nginx"
-  ports {
-    internal = 80
-    external = 8000
-  }
+resource "local_file" "hello_file" {
+  filename = "hello.txt"
+  content  = "Random number: ${random_integer.number.result}"
 }
