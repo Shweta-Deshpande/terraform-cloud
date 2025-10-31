3261ae50fc984a776626ce0d25aa226f8ffe2d34
Shweta Deshpande <shweta.deshpande@hashicorp.com>
2025-10-31 16:10:54 -0700

    Testing with PR3

--- show.tf
+++ show.tf
@@ -1,18 +1,6 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-terraform {
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.0"
-    }
-  }
-}
 resource "random_integer" "number1" {
   min = 1
   max = 10000
