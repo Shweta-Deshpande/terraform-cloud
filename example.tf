483d6410f5ab548fce2c40dd6a87879197afff80
Shweta Deshpande <shweta.deshpande@hashicorp.com>
2025-10-31 17:07:57 -0700

    Testing with PR1

--- example.tf
+++ example.tf
@@ -1,22 +1,9 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-terraform {
-  required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.0"
-    }
-  }
-}
-
 resource "local_file" "example_file" {
   content  = "Hello, Terraform Local Provider!"
   filename = "example.txt"
-}
-
-resource "local_exec" "example_command" {
-  command = "echo 'This command was executed by Terraform local_exec.'"
 }
 
 output "file_content" {
