8996b834cc1a56aa536751acf3d4043738c928d5
Shweta Deshpande <shweta.deshpande@hashicorp.com>
2025-10-31 17:24:30 -0700

    Testing with PR4

--- main.tf
+++ main.tf
@@ -14,10 +14,12 @@ terraform {
   }
 }
 resource "random_integer" "number" {
-  min = 1
+  min = 5
   max = 10000
 }
 resource "local_file" "hello_file" {
   filename = "hello.txt"
   content  = "Random number: ${random_integer.number.result}"
 }
+
+
